c509bd38fd59de653ce67ea5de4c9b4e088252d8
DanukaPraneeth <danuka@wso2telco.com>
2017-04-18 15:29:44 +0530

    branch update 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.print("wso2telco");
-		System.out.println("updated");
-		System.out.println("update 2");
+		System.out.println("Hello world in the branch");
 	}
 
 }
