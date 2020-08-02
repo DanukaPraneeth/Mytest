f757fc00f0c0309238128d8faf6055d7b52cd66a
Danuka <danuka.ariyasinghagamage@singtel.com>
2020-08-02 04:23:17 +0000

    Adding new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Branch update");
 //*********************************//
 		System.out.println("Commit aftere tag-1");
+ 		System.out.println("Create commit from command line");
 	}
 
 }
