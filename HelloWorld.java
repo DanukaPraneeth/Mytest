13bc399142aceac7f48832201867e44f03d48a8e
Danuka Praneeth <a.g.danuka@gmail.com>
2020-11-25 22:43:10 +0800

    Remove print logs

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,15 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world in the branch");
-		System.out.println("Branch update");
-//*********************************//
-		System.out.println("Commit aftere tag-1");
- 		System.out.println("Create commit from command line");
-                System.out.println("Another commit");
-                System.out.println("6th line");
-                System.out.println("7th line");
-//*********************************//		
-                System.out.println("Stash commit");
 
 	}
 
